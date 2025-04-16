a8a47b8660eec0704ac23f582287adf197af1bf2
Khalid Deniev <haliddeniev88@gmail.com>
2025-04-16 14:46:16 +0300

    Разработка

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { createApp } from "@/core";
-import { ExpressServer } from "@/core";
+import { createApp, ExpressServer } from "@/core";
 import { getEnv } from "@/utils";
 
 const express = new ExpressServer({ port: getEnv("PORT") });
